c25798205431dc63bd45a95b35a68078931187ec
tobiaszgithub <tobiasz.github@gmail.com>
2016-03-17 15:12:33 +0100

    Passign python variable to be rendered in the template

--- ch05/superlists/lists/views.py
+++ ch05/superlists/lists/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+        
+    return render(request, 'home.html', {
+                'new_item_text': request.POST.get("item_text", "")
+                })
     
     

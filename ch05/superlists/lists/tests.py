0b64983fe9815dc6ace0444f1d6b9188b4e2589e
tobiaszgithub <tobiasz.github@gmail.com>
2016-03-20 15:55:28 +0100

    Redirect after POST, show all intem in template

--- ch05/superlists/lists/tests.py
+++ ch05/superlists/lists/tests.py
@@ -24,12 +24,44 @@ class HomePageTest(TestCase):
         
         response = home_page(request)
         
-        self.assertIn('A new list item',response.content.decode())
-        expected_html = render_to_string('home.html', {
-                        'new_item_text': request.POST.get('item_text', ''),
-                        }, request=request)
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        newItem = Item.objects.first()
+        self.assertEqual(newItem.text, 'A new list item')
+        
 
+        
+        #=======================================================================
+        # self.assertIn('A new list item',response.content.decode())
+        # expected_html = render_to_string('home.html', {
+        #                 'new_item_text': request.POST.get('item_text', ''),
+        #                 }, request=request)
+        # self.assertEqual(response.content.decode(), expected_html)
+        #=======================================================================
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = 'A new list item'
+        
+        response = home_page(request)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+    
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
+    
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+        
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertIn("item 1", response.content.decode())
+        self.assertIn("item 2", response.content.decode())
+        
+        
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -39,6 +71,7 @@ class ItemModelTest(TestCase):
         second_item = Item()
         second_item.text = 'Item the second'
         second_item.save()
+        
         
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)

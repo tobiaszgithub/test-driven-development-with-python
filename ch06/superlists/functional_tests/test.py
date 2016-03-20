816cdf0df1839071d6fc5fd3eda0d56727524e7b
tobiaszgithub <tobiasz.github@gmail.com>
2016-03-20 18:17:45 +0100

    make functional_tests an app, use LiveServerTestCase

--- ch06/superlists/functional_tests/test.py
+++ ch06/superlists/functional_tests/test.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-import unittest
+from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -55,5 +55,3 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
         
     
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
